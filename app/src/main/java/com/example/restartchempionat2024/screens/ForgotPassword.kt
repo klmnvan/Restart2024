7fce3df69517d68235b517ed64318319a5fadfb8
klmnvan <126891264+klmnvan@users.noreply.github.com>
2024-03-14 13:11:20 +0300

    add verst Forgot Password

--- app/src/main/java/com/example/restartchempionat2024/screens/ForgotPassword.kt
+++ app/src/main/java/com/example/restartchempionat2024/screens/ForgotPassword.kt
@@ -6,16 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.restartchempionat2024.R
-import com.example.restartchempionat2024.databinding.ActivityLogInBinding
+import com.example.restartchempionat2024.databinding.ActivityForgotPasswordBinding
 import com.example.restartchempionat2024.databinding.ActivitySignUpBinding
 
-class SignUp : AppCompatActivity() {
-    private lateinit var binding: ActivitySignUpBinding
+class ForgotPassword : AppCompatActivity() {
+    private lateinit var binding: ActivityForgotPasswordBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
+
 }
