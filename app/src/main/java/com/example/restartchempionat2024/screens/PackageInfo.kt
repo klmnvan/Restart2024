9faac139e47e5d545a7f71d5ab4c14bb5c0b7265
klmnvan <126891264+klmnvan@users.noreply.github.com>
2024-03-18 12:07:51 +0300

    verst check

---
+++ app/src/main/java/com/example/restartchempionat2024/screens/PackageInfo.kt
@@ -0,0 +1,39 @@
+package com.example.restartchempionat2024.screens
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.restartchempionat2024.R
+import com.example.restartchempionat2024.databinding.ActivityPackageInfoBinding
+
+class PackageInfo : AppCompatActivity() {
+    lateinit var binding: ActivityPackageInfoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPackageInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        pressingButton()
+    }
+
+    /** Функция, где обрабатываются нажатия на кнопки */
+    private fun pressingButton() {
+        with(binding) {
+            btnBack.setOnClickListener {
+                startActivity(Intent(this@PackageInfo, Profile::class.java))
+                finish()
+            }
+            btnMakePayment.setOnClickListener {
+                startActivity(Intent(this@PackageInfo, Home::class.java))
+                finish()
+            }
+            btnEditPack.setOnClickListener {
+                finish()
+            }
+        }
+    }
+
+}
